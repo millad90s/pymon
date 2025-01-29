b7c4033dccb053a085a29f716f00a17ab118d329
Milad Saedi <millad90s@gmail.com>
2025-01-29 23:48:36 +0100

    feat: monitor mem and send alert if

---
+++ mem.py
@@ -0,0 +1,36 @@
+### lets monitor memory usage and send alert to email if it is higher than threshould 
+
+import psutil
+import smtplib
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+sender = os.getenv('sender')
+password = os.getenv('password')
+receiver = os.getenv('receiver')
+
+RAM_THRESHOLD = 40
+
+def check_ram_usage():
+    ram_usage = psutil.virtual_memory().percent
+    return ram_usage
+
+def send_email():
+    ram_usage = check_ram_usage()
+    if ram_usage > RAM_THRESHOLD:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(sender, password)
+        subject = 'RAM usage is high'
+        body = f'RAM usage is {ram_usage}%'
+        msg = f'Subject: {subject}\n\n{body}'
+        server.sendmail(sender, receiver, msg)
+        server.quit()
+        debug = f'Email sent to {receiver} with subject {subject} and body {body}'
+        print(debug)
+        
+
+if __name__ == '__main__':
+    send_email()
